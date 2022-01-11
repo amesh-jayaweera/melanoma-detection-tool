e3935fe714a177a548743333fd3eb5ecc431cfcf
Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>
2022-01-11 10:47:54 +0530

    Image Processing added

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from melanoma_detection_pps import detect_melanoma_by_pps, Data
+from melanoma_dermoscopy import checkMelanoma
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/predict": {"origins": "*"}})
@@ -16,6 +17,22 @@ def results():
         response = jsonify(output)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response, 200  # request completed successfully
+    except Exception as e:
+        print(e)
+        return "Bad request", 400  # bad request
+
+@app.route('/predict/image', methods=['POST'])
+def imageResults():
+    try:
+        img = request.files['image']
+        extension = img.filename.split(".")[1]
+        if extension in required_file_extensions:
+            output = checkMelanoma(img)
+            response = jsonify(output)
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response, 200  # request completed successfully
+        else:
+            return "Bad request", 400
     except Exception as e:
         print(e)
         return "Bad request", 400  # bad request
